0e7f76dce0bbe01e9f1a0f7d6faa3c496dc13f1e
ttallman <dspiteself@gmail.com>
2016-08-12 15:54:20 -0500

    commit basic reacthitcher

--- src/hitch/reactjs.cljs
+++ src/hitch/reactjs.cljs
@@ -1,9 +1,77 @@
 (ns hitch.reactjs
   (:require cljsjs.react
-            cljsjs.react.dom))
+            cljsjs.react.dom
+            [hitch.protocols :as proto]
+            [cljs.core.async :as async]
+            [clojure.set]
+            [goog.async.nextTick]
+            [cljs.core.async.impl.protocols :as impl]))
+
 (def ^not-native batchedUpdates
   "The React batchUpdates addon. Takes one function which should call
   forceUpdate (possibly multiple times) inside it. This function will be called
   later and all components updated will have their forceUpdates reordered and
   applied in a single React transaction."
   js/ReactDOM.unstable_batchedUpdates)
+
+
+
+
+(defonce invalidated-react-elements (atom #{}))
+(def queued? false)
+
+(defn subscriber-notify! []
+  (let [subscribers @invalidated-react-elements]
+    (reset! invalidated-react-elements #{})
+    (set! queued? false)
+    (doseq [s subscribers]
+      ;; Could be multimethod/protocol instead (e.g.: -receive-subscription-update)?
+      (when-not (undefined? (.-isMounted s))
+        (when (.isMounted s)                                ;; react component
+          (.forceUpdate s))))))
+(defn flush-invalidated! []
+   (subscriber-notify!))
+
+(defrecord ReactInvalidateWrapper [react-component]
+  proto/ISubscriber
+  (-recalculate! [node graph]
+    (when-not queued?
+      (set! queued? true)
+
+      (goog.async.nextTick flush-invalidated!)
+      )
+    (swap! invalidated-react-elements conj react-component)))
+
+(deftype ReactHitcher [graph react-component ^:mutable requests]
+  proto/IBatching
+  (-request-effect [_ effect]
+    (proto/-request-effect graph effect))
+  (-request-invalidations [_ invalidations]
+    (proto/-request-invalidations graph invalidations))
+  proto/IDependencyGraph
+  (peek-node [this data-selector]
+    (proto/peek-node graph data-selector))
+  (create-node! [this data-selector]
+    (proto/create-node! graph data-selector))
+  (subscribe-node [this data-selector]
+    (set! requests (conj requests data-selector))
+    (let [n (proto/get-or-create-node graph data-selector)
+          changes (proto/node-depend! n (->ReactInvalidateWrapper react-component ))]
+      (when-let [[new-effects new-invalidates] changes]
+        (proto/-request-effect graph new-effects)
+        (proto/-request-invalidations graph new-invalidates))
+      n)))
+
+
+(defn react-hitcher [graph react-component]
+  (->ReactHitcher graph react-component #{}))
+
+(defn new-selectors [oldtx newtx]
+  (if oldtx
+    (clojure.set/difference (.-requests newtx) (.-requests oldtx))
+    newtx))
+
+(defn retired-selectors [oldtx newtx]
+  (if oldtx
+    (clojure.set/difference (.-requests oldtx) (.-requests newtx))
+    #{}))
--- test/hitch/core_test.cljs
+++ test/hitch/core_test.cljs
@@ -2,50 +2,15 @@
   (:require-macros [hitch.eager :refer [go]]
                    [devcards.core :as dc :refer [deftest]])
   (:require [cljs.test :refer [] :refer-macros [is run-tests async]]
+            [hitch.reactjs]
             [hitch.protocols :as proto]
             [hitch.selectors.mutable-var :refer [mutable-var]]
             [hitch.graph :as graph]
             [cljs.core.async :as async]
             [hitch.graphs.mutable :as mgraph]))
 
-
+(def tempob (js-obj))
 (deftest firstt
-         (let [graph (mgraph/graph)]
-              (let [node1 (graph/hook graph mutable-var :test)]
-                   (is (= (async/poll! node1) nil))
-                   (graph/apply-effects graph [[(proto/-selector mutable-var :test) [:set-value 5]]])
-                   (is (= (async/poll! node1) 5))
-                   (is (= (async/poll! (graph/hook graph mutable-var :test)) 5)))))
-
-(deftest firstasync
-         (let [graph (mgraph/graph)]
-              (async done
-                     (let [node1 (graph/hook graph mutable-var :test)
-                           testsel (proto/-selector mutable-var :test)]
-                          (go
-                            (is (= (async/<! node1) 7))
-                            (proto/clear-node! node1 graph)
-                            (is (= (async/<! node1) 8))
-                            (proto/clear-node! node1 graph)
-                            (is (= (async/<! node1) 9))
-                            (done))
-                          (graph/apply-effects graph [[testsel [:set-value 7]]])
-                          (graph/apply-effects graph [[testsel [:set-value 8]]])
-                          (graph/apply-effects graph [[testsel [:set-value 9]]])))))
-
-(deftest take-as-many-as-you-want
-         (let [graph (mgraph/graph)]
-              (async done
-                     (let [node1 (graph/hook graph mutable-var :test)
-                           testsel (proto/-selector mutable-var :test)]
-                          (go
-                            (is (= (async/<! node1) 7))
-                            (is (= (async/<! node1) 7))
-                            (is (= (async/<! node1) 7))
-                            (done)
-                            )
-                          (graph/apply-effects graph [[testsel [:set-value 7]]])
-                          (graph/apply-effects graph [[testsel [:set-value 8]]])
-                          (graph/apply-effects graph [[testsel [:set-value 9]]])))))
-
-
+         (is (= (into #{}
+                      [(hitch.reactjs/->ReactInvalidateWrapper tempob) (hitch.reactjs/->ReactInvalidateWrapper tempob)(hitch.reactjs/->ReactInvalidateWrapper tempob)])
+                #{(hitch.reactjs/->ReactInvalidateWrapper tempob)} )))
