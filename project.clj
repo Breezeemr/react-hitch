5f2e0aebca99160d9a8c6b64c748d65190884f59
ttallman <dspiteself@gmail.com>
2016-10-31 18:23:57 -0500

    update paths in figwheel to dev-resources

--- project.clj
+++ project.clj
@@ -13,37 +13,11 @@
   :profiles {:dev {
                    :dependencies [[com.cemerick/piggieback "0.2.1"]    ; needed by figwheel nrepl
                                   [devcards "0.2.1"]]
-                   :plugins [                               ;[lein-cljsbuild "1.1.1"]
-                             [lein-figwheel "0.5.4-7"]]
-                   :figwheel { :http-server-root "public"
-                              ;; :http-server-root "public" ;; default and assumes "resources"
-                              ;; :server-port 3449 ;; default
-                              :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-                              ;; Start an nREPL server into the running figwheel process
-                              :nrepl-port 7888
-
-                              ;; Server Ring Handler (optional)
-                              ;; if you want to embed a ring handler into the figwheel http-kit
-                              ;; server, this is for simple ring servers, if this
-                              ;; doesn't work for you just run your own server :)
-                              ;; :ring-handler hello_world.server/handler
-
-                              ;; To be able to open files in your editor from the heads up display
-                              ;; you will need to put a script on your path.
-                              ;; that script will have to take a file path and a line number
-                              ;; ie. in  ~/bin/myfile-opener
-                              ;; #! /bin/sh
-                              ;; emacsclient -n +$2 $1
-                              ;;
-                              ;; :open-file-command "myfile-opener"
-
-                              ;; if you want to disable the REPL
-                              ;; :repl false
-
-                              ;; to configure a different figwheel logfile path
-                              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-                              }}}
+                   :plugins        [[lein-figwheel "0.5.4-7"]]
+                   :resource-paths ["dev-resources" "target/devcards"]
+                   :figwheel       {:css-dirs       ["dev-resources/public/css"]
+                                    :nrepl-port     7888
+                                    :server-logfile "target/figwheel-logfile.log"}}}
   :source-paths ["src"]
 
   :clean-targets ^{:protect false} ["resources/public/js" "target"]
@@ -56,8 +30,8 @@
 
                         :compiler     {:main          hitch.test-runner
                                        :asset-path    "js/devcardsout"
-                                       :output-to     "resources/public/js/react_hitch_devcards.js"
-                                       :output-dir    "resources/public/js/devcardsout"
+                                       :output-to     "target/devcards/public/js/react_hitch_devcards.js"
+                                       :output-dir    "target/devcards/public/js/devcardsout"
                                        :optimizations :none
                                        :source-map true
                                        :source-map-timestamp false
@@ -65,7 +39,7 @@
                                        }}
                        {:id           "min"
                         :source-paths ["src"]
-                        :compiler     {:output-to     "resources/public/js/react-hitch.js"
+                        :compiler     {:output-to     "target/devcards/public/js/react-hitch.js"
                                        :main          hitch.reactjs
                                        :optimizations :advanced
                                        :pretty-print  false}}]})
