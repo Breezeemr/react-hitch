6f133f41626e754815114c0a2249a5cd67a10c75
Francis Avila <favila@breezeehr.com>
2018-10-24 13:10:02 -0500

    Add unmount to component instances, effect runner for rerender

--- src/react_hitch/qui_tracker.cljs
+++ src/react_hitch/qui_tracker.cljs
@@ -6,43 +6,65 @@
             [hitch2.protocols.tx-manager :as tx-manager]
             [hitch2.tx-manager.halting :as halting]
             [react-hitch.curator.react-hook :as rh]
-            [quiescent :as q])
-  (:import (goog.async nextTick)))
+            [quiescent :as q]))
 
-(def ^not-native batchedUpdates
-  "The React batchUpdates addon. Takes one function which should call
-  forceUpdate (possibly multiple times) inside it. This function will be called
-  later and all components updated will have their forceUpdates reordered and
-  applied in a single React transaction."
-  js/ReactDOM.unstable_batchedUpdates)
+(defn forceUpdate [c]
+  (when (some? (.-__graph c))
+    (.forceUpdate c))
+  nil)
 
-(defn flush-deps-on-unmount []
+(defn forceUpdate-all [^not-native it]
+  (while (.hasNext it)
+    (forceUpdate (.next it))))
+
+(defn batched-forceUpdate
+  "Takes a :rerender-components effect and updates all components using React's
+  unstable_batchedUpdates."
+  [{:keys [components] :as rerender-component-effect}]
+  (let [it (iter components)]
+    (js/ReactDOM.unstable_batchedUpdates forceUpdate-all it)))
+
+(defn flush-deps-on-unmount {:jsdoc ["@this {*}"]} []
   (this-as c
     (let [graph (.-__graph c)]
       (graph-proto/-transact! graph rh/react-hooker
-                              [:reset-component-parents c #{}]))))
+        [:reset-component-parents c #{}]))))
 
+
+(defn hitchify-component! [c graph]
+  (when-not (some? (.-__graph c))
+    (set! (.-__graph c) graph)
+    (if-some [old-unmount (.-componentWillUnmount c)]
+      (let [new-on-unmount (fn []
+                             (this-as c
+                               (.call old-unmount c)
+                               (.call flush-deps-on-unmount c)))]
+        (set! (.-componentWillUnmount c) new-on-unmount))
+      (set! (.-componentWillUnmount c) flush-deps-on-unmount)))
+  nil)
 
 (defn qui-hitch
   ([graph unresolved c render-fn value services]
+   (hitchify-component! c graph)
    (let [gv              (graph-proto/-get-graph graph)
          rtx             (halting/halting-manager gv)
          result          (halt/maybe-halt
-                          (render-fn value rtx services)
-                          unresolved)
+                           (render-fn value rtx services)
+                           unresolved)
          focus-selectors (tx-manager/finish-tx! rtx)]
      (graph-proto/-transact! graph rh/react-hooker
-                             [:reset-component-parents c focus-selectors])
+       [:reset-component-parents c focus-selectors])
      result))
   ([graph unresolved c render-fn value meta services]
+   (hitchify-component! c graph)
    (let [gv              (graph-proto/-get-graph graph)
          rtx             (halting/halting-manager gv)
          result          (halt/maybe-halt
-                          (render-fn value rtx meta services)
-                          unresolved)
+                           (render-fn value rtx meta services)
+                           unresolved)
          focus-selectors (tx-manager/finish-tx! rtx)]
      (graph-proto/-transact! graph rh/react-hooker
-                             [:reset-component-parents c focus-selectors])
+       [:reset-component-parents c focus-selectors])
      result)))
 
 (defn hitch-render-wrapper [nf]
